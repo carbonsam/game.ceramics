ae6a50c54c871dd8e42025a7a819dff90c56d791
Samuel Schumacher <samschu@live.com>
2022-09-23 17:32:13 -0500

    Make the puck move and bounce

--- 7-Hockey3d/Ball.gd
+++ 7-Hockey3d/Ball.gd
@@ -1,28 +1,29 @@
-extends RigidBody
+extends KinematicBody
 
-export var min_speed = 5.0
-export var max_speed = 30.0
-export var rate_of_speed_increase = 1.0
+export var min_speed = 10.0
+export var max_speed = 75.0
+export var rate_of_speed_increase = 2.5
 
 var current_speed = min_speed
 var velocity = Vector3.ZERO
-var started = false
+var remainder = 0.0
 
 func _ready():
 	randomize()
-	
-	# Pick a random starting direction
-	velocity = Vector3(randf(), 0, randf())
+	# TODO: Randomly change which side the ball moves towards
+	velocity = Vector3(current_speed, 0, current_speed)
 
 func _physics_process(delta):
-	# Speed up on collisions
-#	for i in range(get_slide_count()):
-#		var collision = get_slide_collision(i)
-#		if collision.collider.is_in_group("paddle"):
-#			current_speed += rate_of_speed_increase
-
-	var current_velocity = linear_velocity.normalized()
-	linear_velocity = Vector3(current_velocity.x * current_speed * delta, 0, current_velocity.z * current_speed * delta)
+	var collision = move_and_collide(velocity * delta + velocity.normalized() * remainder)
+	remainder = 0.0
 	
-	if not started:
-		linear_velocity = velocity * current_speed
+	if collision:
+		var speed = velocity.length()
+		var direction = velocity.normalized()
+		var normal = collision.normal
+		
+		if collision.collider.is_in_group("paddle") and speed < max_speed:
+			speed += rate_of_speed_increase
+		
+		remainder = collision.remainder.length()
+		velocity = direction.bounce(normal) * speed
